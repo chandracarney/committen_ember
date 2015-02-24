bb623c1b16d9973a579d18cbb2a98d83a276387a
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-02-24 15:01:57 -0700

    Adds profile editing functionality

---
+++ app/controllers/user.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+
+  editing: false,
+
+  actions: {
+
+    startEditing: function () {
+      this.set('editing', true);
+    },
+
+    stopEditing: function () {
+      var user = this.get('model');
+      user.save().then(function () {
+        this.set('editing', false);
+      }.bind(this));
+    }
+  }
+});
