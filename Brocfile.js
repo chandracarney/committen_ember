ff0d2975cfe41b08b7fbefd47bf844cd2c437cbc
Travis Yoder <trayoda@gmail.com>
2015-03-03 21:59:24 -0700

    broke something with ember and had to add some app.import’s to get it to read fonts again

--- Brocfile.js
+++ Brocfile.js
@@ -2,15 +2,20 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  emberCliFontAwesome: { includeFontAwesomeAssets: false }
+});
 
 app.import('bower_components/foundation/css/normalize.css');
 app.import('bower_components/foundation/css/foundation.css');
 app.import("bower_components/font-awesome/css/font-awesome.css");
+app.import("bower_components/font-awesome/css/font-awesome.css.map");
+app.import("bower_components/font-awesome/css/font-awesome.min.css");
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
 app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
 
 module.exports = app.toTree();
