b550543ac0350c7f07eca840c3c633c60af58993
Chandra Carney <chandracarney@me.com>
2015-03-05 12:24:36 -0700

    Persistent session almost working for Ember GitHub auth

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate: function() {
+    this.get("session").fetch();
+  },
+
   actions: {
     signInViaGithub: function() {
       var route = this;
 
       this.get('session').open('github-oauth2').then(function(){
-        alert('success!');
+        alert('Success! Redirecting you to Dashboard');
+        // do the things after login, like redirect to dashboard
       }, function(error) {
         route.controller.set('error', 'Could not sign you in: ' + error.message);
       });
