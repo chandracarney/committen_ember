74e3079a100504f428925d173db3425d42afd90c
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-03-07 15:25:50 -0700

    Adds create goal functionality

---
+++ app/controllers/goal/new.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  title: null,
+  description: null,
+  startDate: null,
+  endDate: null,
+  dailyQuantity: null,
+  user: null,
+
+  actions: {
+
+    saveGoal: function () {
+      var title = this.get('title');
+      var description = this.get('description');
+      var startDate = this.get('startDate');
+      var endDate = this.get('endDate');
+      var dailyQuantity = this.get('dailyQuantity');
+      var user = this.get('model');
+      var goal = { title: title,
+                   description: description,
+                   startDate: startDate,
+                   endDate: endDate,
+                   dailyQuantity: dailyQuantity,
+                   user: user,
+                  };
+
+      this.store.createRecord('goal', goal).save().then(function(user) {
+        this.transitionToRoute('user', user);
+      }).bind(this);
+    }
+
+  }
+
+});
